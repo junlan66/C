e94844545e5a8da94f1ded9fc7c26da7286b02ee
junlan66 <lu388@purdue.edu>
2015-05-18 12:28:44 -0400

    Better
    Smaller

--- ToLower.c
+++ ToLower.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-  // 'S' 'F' 'E'->'e'
-  char a = '_'; 
-
-  //int c = a+0;
+  
+  char a = 'Y'; 
 
   int b = a+32;
 
-  if ( a > 96 && a <= 122 ) //small
- 
- { printf("%c\n",a);
- } 
-
-  else if ((a > 32 && a < 65) || (a > 90 && a < 97) || (a > 122 && a < 128)) //others
+  if ((a >= 1 && a < 65) || (a > 90 && a < 128)) //small and others
  {
  printf("%c\n",a);
  } else 
